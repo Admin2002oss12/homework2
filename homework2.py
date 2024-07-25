d2cb54e3b5ea6e8cc6deb65d0caff2e6e0eff5a5
Кремнев Кирилл <77299752+Admin2002oss12@users.noreply.github.com>
2024-07-25 18:01:47 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_of_DZ = 12
+number_of_hours_spend = 1.5
+course_name = 'Python'
+time_per_task = number_of_hours_spend / number_of_DZ
+#print('всего задач:', number_of_DZ)
+print('Курс:', course_name, 'всего задач:', number_of_DZ, 'затрачено часов:', number_of_hours_spend, 'среднее время выполнения', time_per_task, 'часа' )
